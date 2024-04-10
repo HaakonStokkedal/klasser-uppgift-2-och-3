53305c98454dc171e1e729cd4d3d05d9a341ab2c
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-04-10 13:13:29 +0200

    Add project files.

---
+++ klasser uppgift 2 och 3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace klasser_uppgift_2_och_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            personuppgifter p1 = new personuppgifter();
+            p1.MataIn();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ klasser uppgift 2 och 3/personuppgifter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_uppgift_2_och_3
+{
+    internal class personuppgifter
+    {
+        public string förnamn;
+        public string efternamn;
+        public int ålder;
+
+        public void MataIn()
+        {
+            Console.WriteLine("Vad heter du i förnamn?");
+            förnamn = Console.ReadLine();
+            Console.WriteLine("Vad heter du i efternamn?");
+            efternamn = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Hej {förnamn} {efternamn}!");
+            Console.WriteLine("Hur gammal är du?");
+            ålder = int.Parse(Console.ReadLine());
+
+            if (Myndig() == true)
+            {
+                Console.WriteLine("Du är myndig!!!");
+            }
+            else
+            {
+                Console.WriteLine("Du är inte mydnig :(");
+            }
+        }
+
+        public bool Myndig()
+        {
+            if (ålder >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+
+}
